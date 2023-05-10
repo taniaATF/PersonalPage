db57c16bb26bb9864baa0e374b7f571ffed9cfbe
P1antain <Front@End06121987>
2023-05-10 19:39:36 +0300

    fix

--- src/app/news/page.js
+++ src/app/news/page.js
@@ -7,8 +7,8 @@ export default function Page() {
         <div className="news__wrapper">
           <a href="https://infourok.ru/user/novozhilova-tatyana-fyodorovna" target={"_blank"} rel={'nofollow'}>Я на Инфоуроке</a>
           <a href="https://vk.com/id220220236" target={"_blank"} rel={'nofollow'}>Я Вконтакте</a>
-          <a href=" https://vk.com/4ttut" target={"_blank"} rel={'nofollow'}>ГБПОУ “ЧТТИТ” в Вконтакте</a>
-          <a href="http://протехникум.рф/blog/index.php?category=0" target={"_blank"} rel={'nofollow'}> ГБПОУ “ЧТТИТ”</a>
+          <a href=" https://vk.com/4ttut" target={"_blank"} rel={'nofollow'}>ГБПОУ &quot;ЧТТИТ&quot; в Вконтакте</a>
+          <a href="http://протехникум.рф/blog/index.php?category=0" target={"_blank"} rel={'nofollow'}> ГБПОУ &quot;ЧТТИТ&quot;</a>
         </div>
       </section>
     </>
